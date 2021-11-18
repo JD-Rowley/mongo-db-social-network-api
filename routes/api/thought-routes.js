1816d500e460545173c720351b70a10c7cf9bc00
JD Rowley <jdrowley1989@gmail.com>
2021-11-18 16:40:38 -0700

    added functional thought routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,3 +1,27 @@
 const router = require('express').Router();
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought
+} = require('../../controllers/thought-controller');
+
+router
+    .route('/')
+    .get(getAllThoughts);
+
+router
+    .route('/:userId')
+    .post(createThought);
+
+router
+    .route('/:id')
+    .get(getThoughtById);
+
+router
+    .route('/:userId/:id')
+    .put(updateThought)
+    .delete(deleteThought);
 
 module.exports = router;
